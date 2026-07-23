32217c25e715d6ac7e44b2cdbbcc6e1f9821c1d5
alfoo <alfoo@AHMED>
2026-07-23 06:03:33 +0400

    Add project files.

---
+++ banking_mangament/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace banking_mangament
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
